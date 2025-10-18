d709929bb065e604fe40f2061089db682e68ae93
chrisrahi9 <christrahi16@gmail.com>
2025-10-19 00:44:12 +0200

    Update layout.tsx

--- stackblitz-starters-n2un17vw/app/layout.tsx
+++ stackblitz-starters-n2un17vw/app/layout.tsx
@@ -1,5 +1,12 @@
 import './globals.css'
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#004642" },
+    { media: "(prefers-color-scheme: dark)",  color: "#004642" },
+  ],
+};
 export const metadata = {
   title: 'Voskopulence',
   description: 'Mediterranean luxury haircare',
