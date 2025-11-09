3ec12afa71f066ef5a24e7315e5076275d5c5542
chrisrahi9 <christrahi16@gmail.com>
2025-11-10 00:00:28 +0100

    Create head.tsx

---
+++ stackblitz-starters-n2un17vw/app/head.tsx
@@ -0,0 +1,12 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+      <meta name="theme-color" content="#004642" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      <meta name="format-detection" content="telephone=no" />
+    </>
+  );
+}
