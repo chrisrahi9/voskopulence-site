4d8ccb3ef20c35da7b79a2fc9a2f7da95690b03b
chrisrahi9 <christrahi16@gmail.com>
2026-08-27 13:24:29 +0200

    Bypass broken media proxy via CDN redirects

--- stackblitz-starters-n2un17vw/next.config.js
+++ stackblitz-starters-n2un17vw/next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/media/hero_hls/1080_only.m3u8",
+        destination: "https://cdn.voskopulence.com/hero_hls/master.m3u8",
+        permanent: false,
+      },
+      {
+        source: "/media/:path*",
+        destination: "https://cdn.voskopulence.com/:path*",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
