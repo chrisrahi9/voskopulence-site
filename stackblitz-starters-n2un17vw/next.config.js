1ecf81f75930c9a35232d951c6f12e3560ef8778
chrisrahi9 <christrahi16@gmail.com>
2026-08-27 15:33:30 +0200

    Route media through Bunny hostname with valid TLS

--- stackblitz-starters-n2un17vw/next.config.js
+++ stackblitz-starters-n2un17vw/next.config.js
@@ -4,12 +4,12 @@ const nextConfig = {
     return [
       {
         source: "/media/hero_hls/1080_only.m3u8",
-        destination: "https://cdn.voskopulence.com/hero_hls/master.m3u8",
+        destination: "https://vosko-cdn.b-cdn.net/hero_hls/1080p/playlist.m3u8",
         permanent: false,
       },
       {
         source: "/media/:path*",
-        destination: "https://cdn.voskopulence.com/:path*",
+        destination: "https://vosko-cdn.b-cdn.net/:path*",
         permanent: false,
       },
     ];
