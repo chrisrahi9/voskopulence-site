3803dc93c5c30829af3aa3fa843c30fc7d5a2cf3
chrisrahi9 <christrahi16@gmail.com>
2026-09-07 21:24:53 +0200

    Enable premium responsive image formats

--- stackblitz-starters-n2un17vw/next.config.js
+++ stackblitz-starters-n2un17vw/next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1600, 1920],
+    imageSizes: [256, 320, 384, 480],
+  },
   async redirects() {
     return [
       {
