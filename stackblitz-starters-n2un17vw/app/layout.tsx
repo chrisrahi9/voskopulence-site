6dcea3d96ced5f7f307b0e9a4acff71eea15c6a1
chrisrahi9 <christrahi16@gmail.com>
2026-09-12 14:50:22 +0200

    Remove runtime video speed controller

--- stackblitz-starters-n2un17vw/app/layout.tsx
+++ stackblitz-starters-n2un17vw/app/layout.tsx
@@ -6,7 +6,6 @@ import ScrollToTop from "./_components/ScrollToTop";
 import PremiumMotionController from "./_components/PremiumMotionController";
 import LanguageController from "./_components/LanguageController";
 import CurtainGestureController from "./_components/CurtainGestureController";
-import HeroPlaybackController from "./_components/HeroPlaybackController";
 
 const leagueSpartan = League_Spartan({
   subsets: ["latin"],
@@ -94,7 +93,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <PremiumMotionController />
         <LanguageController />
         <CurtainGestureController />
-        <HeroPlaybackController />
         {children}
       </body>
     </html>
