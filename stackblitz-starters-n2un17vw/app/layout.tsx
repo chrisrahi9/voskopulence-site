127271a3c49c606e0fd2c6fbcb874a18876e3776
chrisrahi9 <christrahi16@gmail.com>
2026-09-14 10:10:23 +0200

    Wire poster-overlay hero reveal trial

--- stackblitz-starters-n2un17vw/app/layout.tsx
+++ stackblitz-starters-n2un17vw/app/layout.tsx
@@ -7,6 +7,7 @@ import PremiumMotionController from "./_components/PremiumMotionController";
 import LanguageController from "./_components/LanguageController";
 import CurtainGestureController from "./_components/CurtainGestureController";
 import HeroDiagnostics from "./_components/HeroDiagnostics";
+import HeroPosterOverlayReveal from "./_components/HeroPosterOverlayReveal";
 
 const leagueSpartan = League_Spartan({
   subsets: ["latin"],
@@ -94,6 +95,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <PremiumMotionController />
         <LanguageController />
         <CurtainGestureController />
+        <HeroPosterOverlayReveal />
         <HeroDiagnostics />
         {children}
       </body>
